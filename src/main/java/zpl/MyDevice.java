6bcf5452748c53e7348b807ebb13adce7677583a
liujiawei <liu-jiawei@jfrai.com>
2020-04-17 11:49:30 +0800

    提交

---
+++ src/main/java/zpl/MyDevice.java
@@ -0,0 +1,40 @@
+package zpl;
+
+public class MyDevice {
+    private String number;
+    private String deviceName;
+    private String userName;
+    private String buyTime;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getBuyTime() {
+        return buyTime;
+    }
+
+    public void setBuyTime(String buyTime) {
+        this.buyTime = buyTime;
+    }
+}
